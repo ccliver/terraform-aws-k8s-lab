541404d496a1209b076ee9d200654f0ca9c17bff
Carl Cliver <ccliver@gmail.com>
2026-04-07 19:18:02 -0400

    fix: fixing efs SG and updating pre-commit

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -102,7 +102,7 @@ resource "aws_security_group" "efs" {
     from_port       = 2049
     to_port         = 2049
     protocol        = "tcp"
-    security_groups = [module.eks.cluster_security_group_id]
+    security_groups = [module.eks.node_security_group_id]
   }
 }
 
